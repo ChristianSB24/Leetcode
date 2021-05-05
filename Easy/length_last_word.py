f8756f1669d81658ca995ec24d5e55696b6c02f6
ChristianSB24 <christiansaldana10@gmail.com>
2021-05-04 23:07:20 -0500

    Added solutions for length of last word and max sub problems

---
+++ Easy/length_last_word.py
@@ -0,0 +1,44 @@
+""" Given a string s consists of some words separated by spaces, return the length of the last word in the string. 
+    If the last word does not exist, return 0.
+
+A word is a maximal substring consisting of non-space characters only. """
+
+# def lenWord(s):
+#     length = 0
+#     prev = False
+#     for i in range(len(s)):
+#         if s[-1 - i] != ' ':
+#             prev = True
+#             length += 1
+#         if s[-1 - i] == ' ' and prev == True:
+#             return length
+#         if s[-1 - i] == ' ':
+#             continue
+#     return length 
+
+# def lenWord(s):
+#     length = []
+#     prev = False
+#     s = s[::-1]
+#     for i in range(len(s)):
+#         if s[i] != ' ':
+#             length.append(s[i])
+#             prev = True
+#         if s[i] == ' ' and prev == True:
+#             break
+#     return len(length)
+
+def lenWord(s):
+    s_list = s.strip().split(" ")
+    print(s_list)
+    l = len(s_list[len(s_list)-1])
+    return l
+        
+        
+
+s = "Hello World    "
+# s = " "
+# s = "a"
+# s = "a "
+
+print(lenWord(s))
---
+++ Easy/max_sub.py
@@ -0,0 +1,27 @@
+""" Given an integer array nums, find the contiguous subarray (containing at least one number) 
+    which has the largest sum and return its sum."""
+
+def maxSub(nums):
+    sums = []
+    if len(nums) == 1:
+        return nums[0]
+    if all(x > -1 for x in nums):
+        print('here')
+        return sum(nums)
+    for i in range(len(nums)):
+        counter = 1
+        for x in range(len(nums)):
+            if nums[i: x + counter] == []:
+                continue
+            sums.append(sum(nums[i:x + counter]))
+        counter += 1
+    return max(sums)
+
+
+
+# nums = [-2,1,-3,4,-1,2,1,-5,4]
+# nums = [1]
+# nums = [5,4,-1,7,8]
+# nums = [-2,-1]
+nums = [1, 0, 3, 4, 6]
+print(maxSub(nums))
