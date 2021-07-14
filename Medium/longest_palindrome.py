07d948f0319b222459c87eb08b61171cb790dbcf
ChristianSB24 <christiansaldana10@gmail.com>
2021-07-13 20:24:21 -0500

    Added solution for longest_palindrome

--- Medium/longest_palindrome.py
+++ Medium/longest_palindrome.py
@@ -1,10 +1,27 @@
 """ Given a string s, return the longest palindromic substring in s. """ 
 
-
-def longPal(s):
-    dic = {}
-    
-
+class Solution(object):
+    def longPal(self, s):
+        res = ""
+        for i in range(len(s)):
+            # odd case, like "aba"
+            tmp = self.helper(s, i, i)
+            if len(tmp) > len(res):
+                res = tmp
+            # even case, like "abba"
+            tmp = self.helper(s, i, i+1)
+            if len(tmp) > len(res):
+                res = tmp
+        return res
+ 
+    # get the longest palindrome, l, r are the middle indexes   
+    # from inner to outer
+    def helper(self, s, l, r):
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l -= 1; r += 1
+        return s[l+1:r]
 
 
 s = "babad"
+ls = Solution()
+print(ls.longPal(s))
